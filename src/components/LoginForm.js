d5aa4299532add6e8ec8f6ec8eaae481de1c80cc
Dominik <dominik.szlaza@gazeta.pl>
2022-08-17 09:03:40 +0200

    Refactored login page to containers/presenters pattern

---
+++ src/components/LoginForm.js
@@ -0,0 +1,81 @@
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Formik } from "formik";
+
+const LoginForm = ({ initialValues, validationSchema, handleFormSubmit }) => {
+  return (
+    <div>
+      <h2 className="mb-3 text-center">Zaloguj się</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleFormSubmit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          isSubmitting,
+        }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="formEmail" className="mb-2">
+              <Form.Label hidden>Adres e-mail</Form.Label>
+              <Form.Control
+                name="email"
+                type="email"
+                placeholder="Adres e-mail"
+                value={values.email}
+                disabled={isSubmitting}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && errors?.email}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors?.email}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="formPassword" className="mb-2">
+              <Form.Label hidden>Hasło</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Hasło"
+                autoComplete="off"
+                value={values.password}
+                disabled={isSubmitting}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.password && errors?.password}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors?.password}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Link to="/resetuj-haslo" className="d-block mb-2 link-primary">
+              Zapomniałem hasła
+            </Link>
+            <Button
+              variant="primary"
+              type="submit"
+              disabled={isSubmitting}
+              className="w-100"
+            >
+              Zaloguj się
+            </Button>
+          </Form>
+        )}
+      </Formik>
+      <p className="mt-3 mb-2">Nie masz jeszcze konta?</p>
+      <Link to="/rejestracja">
+        <Button variant="outline-primary" className="w-100">
+          Załóż konto
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default LoginForm;
