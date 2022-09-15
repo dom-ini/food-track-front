2244e15f1e3e778f65368d0a89604522b7a72ae1
Dominik <dominik.szlaza@gazeta.pl>
2022-09-15 21:41:14 +0200

    Added transitions on route change

--- src/layouts/LoggedInLayout.js
+++ src/layouts/LoggedInLayout.js
@@ -1,5 +1,7 @@
 import { Container } from "react-bootstrap";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { TransitionGroup } from "react-transition-group";
 
 import Header from "../layouts/Header";
 import Footer from "../layouts/Footer";
@@ -7,11 +9,22 @@ import Footer from "../layouts/Footer";
 import "../styles/layouts/LoggedInLayout.scss";
 
 const LoggedInLayout = () => {
+  const location = useLocation();
+
   return (
     <>
       <Header />
       <Container className="content-wrapper">
-        <Outlet />
+        <TransitionGroup>
+          <CSSTransition
+            key={location.pathname}
+            classNames="appear-from-left"
+            timeout={300}
+            exit={false}
+          >
+            <Outlet />
+          </CSSTransition>
+        </TransitionGroup>
       </Container>
       <Footer />
     </>
--- src/layouts/NotLoggedInLayout.js
+++ src/layouts/NotLoggedInLayout.js
@@ -1,11 +1,15 @@
 import { Row, Col, Container } from "react-bootstrap";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import { TransitionGroup } from "react-transition-group";
 
 import Logo from "../components/Logo";
 
 import "../styles/layouts/NotLoggedInLayout.scss";
 
 const NotLoggedInLayout = () => {
+  const location = useLocation();
+
   return (
     <Container className="pt-5">
       <Row className="box-wrapper mx-auto">
@@ -13,7 +17,16 @@ const NotLoggedInLayout = () => {
           <Link to="/logowanie">
             <Logo height="100" width="212" className="d-block mx-auto mb-3" />
           </Link>
-          <Outlet />
+          <TransitionGroup>
+            <CSSTransition
+              key={location.pathname}
+              classNames="appear-from-left"
+              timeout={300}
+              exit={false}
+            >
+              <Outlet />
+            </CSSTransition>
+          </TransitionGroup>
         </Col>
       </Row>
     </Container>
